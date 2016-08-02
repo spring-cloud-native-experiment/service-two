89c3a69e9df59105410d030b12ff5a23aad908dd
Praveer Gupta <praveerg@inpraveerg.local>
2016-08-02 11:43:29 +0530

    Add support for config-server property

--- src/main/java/com/example/ServiceTwoController.java
+++ src/main/java/com/example/ServiceTwoController.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,10 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.Map;
 
+@RefreshScope
 @RestController
 public class ServiceTwoController {
+
+    private final String message;
+
+    public ServiceTwoController(@Value("${message}") String message) {
+        this.message = message;
+    }
+
     @RequestMapping(value = "/greet", method = RequestMethod.GET)
     public @ResponseBody Map<String, String> greet() {
         return Collections.singletonMap("hello", "world");
+    }
+
+    @RequestMapping(value = "message", method = RequestMethod.GET)
+    public @ResponseBody Map<String, String> message() {
+        return Collections.singletonMap("message", message);
     }
 }
